75833dbb19b6633fb0741df88d304b141638a05d
Your Name <you@example.com>
2025-06-26 12:18:37 +0200

    AuthorController, GenreController and ReservationController tests. We've hit 30% coverage

---
+++ app/tests/Controller/AuthorControllerTest.php
@@ -0,0 +1,186 @@
+<?php
+
+/**
+ * Author controller test.
+ */
+
+namespace App\Tests\Controller;
+
+use App\Entity\Author;
+use App\Entity\Enum\UserRole;
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\Exception\ORMException;
+use Doctrine\ORM\OptimisticLockException;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+/**
+ * Class AuthorControllerTest.
+ */
+class AuthorControllerTest extends WebTestCase
+{
+    /**
+     * Default test route.
+     *
+     * @const string
+     */
+    public const TEST_ROUTE = '/ebay/authors';
+
+    /**
+     * Create test route.
+     *
+     * @const string
+     */
+    public const TEST_ROUTE_CREATE = '/ebay/authors/create';
+
+    /**
+     * Test client.
+     */
+    private KernelBrowser $httpClient;
+
+    /**
+     * Set up tests.
+     */
+    public function setUp(): void
+    {
+        $this->httpClient = static::createClient();
+    }
+
+    /**
+     * Test index route for anonymous user.
+     */
+    public function testIndexRouteAnonymousUser(): void
+    {
+        // given
+        $expectedStatusCode = 302;
+
+        // when
+        $this->httpClient->request('GET', self::TEST_ROUTE);
+        $resultStatusCode = $this->httpClient->getResponse()->getStatusCode();
+
+        // then
+        $this->assertEquals($expectedStatusCode, $resultStatusCode);
+    }
+
+    /**
+     * Test index route for admin user.
+     *
+     * @throws ContainerExceptionInterface|NotFoundExceptionInterface|ORMException|OptimisticLockException
+     */
+    public function testIndexRouteAdminUser(): void
+    {
+        // given
+        $expectedStatusCode = 200;
+        $user = $this->createUser([UserRole::ROLE_USER->value, UserRole::ROLE_ADMIN->value]);
+        $this->httpClient->loginUser($user);
+
+        // when
+        $this->httpClient->request('GET', self::TEST_ROUTE_CREATE);
+        $resultStatusCode = $this->httpClient->getResponse()->getStatusCode();
+
+        // then
+        $this->assertEquals($expectedStatusCode, $resultStatusCode);
+    }
+
+    /**
+     * Test index route for anonymous user.
+     */
+    public function testCreateRouteAnonymousUser(): void
+    {
+        // given
+        $expectedStatusCode = 302;
+
+        // when
+        $this->httpClient->request('GET', self::TEST_ROUTE_CREATE);
+        $resultStatusCode = $this->httpClient->getResponse()->getStatusCode();
+
+        // then
+        $this->assertEquals($expectedStatusCode, $resultStatusCode);
+    }
+
+    /**
+     * Edit route test.
+     *
+     * @throws OptimisticLockException
+     * @throws NotFoundExceptionInterface
+     * @throws ORMException
+     * @throws ContainerExceptionInterface
+     */
+    public function testEditRoute(): void
+    {
+        $user = $this->createUser([UserRole::ROLE_USER->value, UserRole::ROLE_ADMIN->value]);
+        $this->httpClient->loginUser($user);
+
+        $authorId = $this->createAuthor()->getId();
+
+        $this->httpClient->request('GET', '/ebay/authors/'.$authorId.'/edit');
+
+        $this->assertResponseIsSuccessful();
+    }
+
+    /**
+     * Delete route test.
+     *
+     * @throws OptimisticLockException
+     * @throws NotFoundExceptionInterface
+     * @throws ORMException
+     * @throws ContainerExceptionInterface
+     */
+    public function testDeleteRoute(): void
+    {
+        $user = $this->createUser([UserRole::ROLE_USER->value, UserRole::ROLE_ADMIN->value]);
+        $this->httpClient->loginUser($user);
+
+        $authorId = $this->createAuthor()->getId();
+
+        $this->httpClient->request('GET', '/ebay/authors/'.$authorId.'/delete');
+
+        $this->assertResponseIsSuccessful();
+    }
+
+    /**
+     * Create fake user.
+     *
+     * @param array $roles User roles
+     *
+     * @return User User entity
+     *
+     * @throws ContainerExceptionInterface|NotFoundExceptionInterface|ORMException|OptimisticLockException
+     */
+    private function createUser(array $roles): User
+    {
+        $passwordHasher = static::getContainer()->get('security.password_hasher');
+        $user = new User();
+        $user->setEmail('user@example.com');
+        $user->setRoles($roles);
+        $user->setPassword(
+            $passwordHasher->hashPassword(
+                $user,
+                'p@55w0rd'
+            )
+        );
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        $userRepository->save($user);
+
+        return $user;
+    }
+
+    /**
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
+     */
+    private function createAuthor(): Author
+    {
+        $entityManager = static::getContainer()->get('doctrine')->getManager();
+
+        $author = new Author();
+        $author->setAlias('Test Author');
+        $entityManager->persist($author);
+        $entityManager->flush();
+
+        return $author;
+    }
+}
---
+++ app/tests/Controller/ReservationControllerTest.php
@@ -0,0 +1,222 @@
+<?php
+
+/**
+ * Reservation controller test.
+ */
+
+namespace App\Tests\Controller;
+
+use App\Entity\Author;
+use App\Entity\Enum\UserRole;
+use App\Entity\Genre;
+use App\Entity\Record;
+use App\Entity\Reservation;
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\Exception\ORMException;
+use Doctrine\ORM\OptimisticLockException;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+/**
+ * Class ReservationControllerTest.
+ */
+class ReservationControllerTest extends WebTestCase
+{
+    /**
+     * Default test route.
+     *
+     * @const string
+     */
+    public const TEST_ROUTE = '/ebay/reservations';
+
+    /**
+     * Create test route.
+     *
+     * @const string
+     */
+    public const TEST_ROUTE_CREATE = '/ebay/reservations/create';
+
+    /**
+     * Test client.
+     */
+    private KernelBrowser $httpClient;
+
+    /**
+     * Set up tests.
+     */
+    public function setUp(): void
+    {
+        $this->httpClient = static::createClient();
+    }
+
+    /**
+     * Test index route for anonymous user.
+     */
+    public function testIndexRouteAnonymousUser(): void
+    {
+        // given
+        $expectedStatusCode = 302;
+
+        // when
+        $this->httpClient->request('GET', self::TEST_ROUTE);
+        $resultStatusCode = $this->httpClient->getResponse()->getStatusCode();
+
+        // then
+        $this->assertEquals($expectedStatusCode, $resultStatusCode);
+    }
+
+    /**
+     * Test index route for admin user.
+     *
+     * @throws ContainerExceptionInterface|NotFoundExceptionInterface|ORMException|OptimisticLockException
+     */
+    public function testIndexRouteAdminUser(): void
+    {
+        // given
+        $expectedStatusCode = 200;
+        $user = $this->createUser([UserRole::ROLE_USER->value, UserRole::ROLE_ADMIN->value]);
+        $this->httpClient->loginUser($user);
+
+        // when
+        $this->httpClient->request('GET', self::TEST_ROUTE_CREATE);
+        $resultStatusCode = $this->httpClient->getResponse()->getStatusCode();
+
+        // then
+        $this->assertEquals($expectedStatusCode, $resultStatusCode);
+    }
+
+    /**
+     * Test index route for anonymous user.
+     */
+    public function testCreateRouteAnonymousUser(): void
+    {
+        // given
+        $expectedStatusCode = 200;
+
+        // when
+        $this->httpClient->request('GET', self::TEST_ROUTE_CREATE);
+        $resultStatusCode = $this->httpClient->getResponse()->getStatusCode();
+
+        // then
+        $this->assertEquals($expectedStatusCode, $resultStatusCode);
+    }
+
+    /**
+     * Show route test.
+     *
+     * @throws OptimisticLockException
+     * @throws NotFoundExceptionInterface
+     * @throws ORMException
+     * @throws ContainerExceptionInterface
+     */
+    public function testShowRoute(): void
+    {
+        $entityManager = static::getContainer()->get('doctrine')->getManager();
+
+        $user = $this->createUser([UserRole::ROLE_USER->value, UserRole::ROLE_ADMIN->value]);
+        $this->httpClient->loginUser($user);
+
+        $reservation = new Reservation();
+        $reservation->setEmail('example@example.com');
+        $reservation->setNick('Test Nick');
+        $reservation->setContent('Test Content');
+        $reservation->setRecord($this->createRecordOwnedBy($user));
+        $entityManager->persist($reservation);
+        $entityManager->flush();
+
+        $this->httpClient->request('GET', '/ebay/reservations/'.$reservation->getId());
+
+        $this->assertResponseIsSuccessful();
+    }
+
+    /**
+     * Delete route test.
+     *
+     * @throws OptimisticLockException
+     * @throws NotFoundExceptionInterface
+     * @throws ORMException
+     * @throws ContainerExceptionInterface
+     */
+    public function testDeleteRoute(): void
+    {
+        $entityManager = static::getContainer()->get('doctrine')->getManager();
+
+        $user = $this->createUser([UserRole::ROLE_USER->value, UserRole::ROLE_ADMIN->value]);
+        $this->httpClient->loginUser($user);
+
+        $reservation = new Reservation();
+        $reservation->setEmail('example@example.com');
+        $reservation->setNick('Test Nick');
+        $reservation->setContent('Test Content');
+        $reservation->setRecord($this->createRecordOwnedBy($user));
+        $entityManager->persist($reservation);
+        $entityManager->flush();
+
+        $this->httpClient->request('GET', '/ebay/reservations/'.$reservation->getId().'/delete');
+
+        $this->assertResponseIsSuccessful();
+    }
+
+    /**
+     * Create fake user.
+     *
+     * @param array $roles User roles
+     *
+     * @return User User entity
+     *
+     * @throws ContainerExceptionInterface|NotFoundExceptionInterface|ORMException|OptimisticLockException
+     */
+    private function createUser(array $roles): User
+    {
+        $passwordHasher = static::getContainer()->get('security.password_hasher');
+        $user = new User();
+        $user->setEmail('user@example.com');
+        $user->setRoles($roles);
+        $user->setPassword(
+            $passwordHasher->hashPassword(
+                $user,
+                'p@55w0rd'
+            )
+        );
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        $userRepository->save($user);
+
+        return $user;
+    }
+
+    /**
+     * Create fake record.
+     *
+     * @param User $user
+     * @return Record
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
+     */
+    private function createRecordOwnedBy(User $user): Record
+    {
+        $entityManager = static::getContainer()->get('doctrine')->getManager();
+
+        $genre = new Genre();
+        $genre->setGenreName('Test Genre');
+        $entityManager->persist($genre);
+
+        $author = new Author();
+        $author->setAlias('Test Author');
+        $entityManager->persist($author);
+
+        $record = new Record();
+        $record->setTitle('Test Record');
+        $record->setAuthor($author);
+        $record->setGenre($genre);
+        $record->setRental($user);
+        $record->setInStock(1);
+
+        $entityManager->persist($record);
+        $entityManager->flush();
+
+        return $record;
+    }
+}
